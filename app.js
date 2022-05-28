dc999ad1f01200303a0c9f39af831aa5b3c77f33
Hanif Maliki <hanifmaliki97@gmail.com>
2022-05-29 00:17:24 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const { getFavBooks, insertFavBooks, deleteFavBooks } = require('./function.js')
+
+const express = require('express')
+// const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', async (req, res) => {
+    res.json({
+        message: "Halo gaes, ini endpoint."
+    })
+})
+
+app.get('/getFavBooks', async (req, res) => {
+    let data = await getFavBooks().catch(console.dir);
+    res.json(data)
+})
+
+app.get('/insertFavBooks', async (req, res) => {
+    let data = await insertFavBooks(req.query).catch(console.dir);
+    res.json(data)
+})
+
+app.get('/deleteFavBooks', async (req, res) => {
+    let data = await deleteFavBooks(req.query).catch(console.dir);
+    res.json(data)
+})
+
+const PORT = 8000
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
---
+++ function.js
@@ -0,0 +1,71 @@
+const { MongoClient } = require('mongodb')
+
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri = "mongodb+srv://hanifmaliki:DAUULmjKAEEtoZN5@hanifcluster.uwjchy9.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+const database = client.db("hanifdb");
+const fav_books = database.collection("fav_books");
+
+async function getFavBooks() {
+    try {
+        await client.connect();
+
+        const query = {};
+        const options = {
+            sort: { title: 1 },
+            projection: { _id: 0, id: 1, title: 1, link: 1, authors: 1, rating: 1 },
+        };
+
+        const cursor = fav_books.find(query, options);
+
+        const result = await cursor.toArray();
+
+        return result
+    } finally {
+        await client.close();
+    }
+}
+
+async function insertFavBooks(inputs) {
+    try {
+        await client.connect();
+
+        const doc = {
+            id: inputs.title,
+            title: inputs.title,
+            link: inputs.link,
+            authors: inputs.authors,
+            rating: inputs.rating
+        }
+        const result = await fav_books.insertOne(doc);
+
+        const response = { message: `A document was inserted with the _id: ${result.insertedId}` }
+
+        return response
+    } finally {
+        await client.close();
+    }
+}
+
+async function deleteFavBooks(input) {
+    try {
+        await client.connect();
+
+        const query = { id: input.id };
+        const result = await fav_books.deleteOne(query);
+
+        if (result.deletedCount === 1) {
+            return { message: "Successfully deleted one document." }
+        }
+
+        return { message: "No documents matched the query. Deleted 0 documents." }
+    } finally {
+        await client.close();
+    }
+}
+
+module.exports = {
+    getFavBooks: getFavBooks,
+    insertFavBooks: insertFavBooks,
+    deleteFavBooks: deleteFavBooks,
+};
